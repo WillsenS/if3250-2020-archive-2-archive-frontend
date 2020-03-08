d407730a54bb62819b5d2f8b91598865e551b7a3
harrymunli <prgmrharry@gmail.com>
2020-03-08 18:26:10 +0700

    add responsiveness

--- pages/detail.js
+++ pages/detail.js
@@ -5,7 +5,7 @@ import Button from '@material-ui/core/Button'
 import sampleDocument from '../public/static/img/sample-document.png';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import theme from '../theme/index';
+import theme from '../theme/home';
 import { Typography } from '@material-ui/core';
 
 const DetailStyles = makeStyles(theme =>({
@@ -13,10 +13,18 @@ const DetailStyles = makeStyles(theme =>({
     padding : "16px"
   },
   title2: {
-    width: "250px"
+    flexShrink: "0",
+    [theme.breakpoints.up('md')]:{
+      width: "250px"
+    },
+    [theme.breakpoints.between('sm','md')]:{
+      width: "150px"
+    }
   },
   oneLineContainer: {
-    display: 'flex'
+    [theme.breakpoints.up('sm')]:{
+      display: 'flex'
+    }
   },
   archiveImg: {
     display: "block", 
@@ -29,7 +37,15 @@ const DetailStyles = makeStyles(theme =>({
     display: 'flex',
     justifyContent: 'center',
     flexDirection: 'column',
-    margin: '16px 200px'
+    [theme.breakpoints.up('md')]:{
+      margin: '16px 200px'
+    },
+    [theme.breakpoints.between('sm','md')]:{
+      margin: '16px 100px'
+    },
+    [theme.breakpoints.down('xs')]:{
+      margin: '16px 16px'
+    }
   },
   buttonArea: {
     marginTop: '16px',
@@ -38,7 +54,7 @@ const DetailStyles = makeStyles(theme =>({
     marginRight: "16px"
   },
   buttonEdit: {
-    marginLeft: "75.3%"
+    float: "right"
   }
 }));
 
