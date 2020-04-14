8a9623c34ead257a1f8539f4049dabd582c5b5bc
Juniardi Akbar <13517075@std.stei.itb.ac.id>
2020-04-14 22:10:39 +0700

    Update index.js

--- config/index.js
+++ config/index.js
@@ -2,12 +2,9 @@ const config = {
   // defaultURL: "http://localhost:3001",
   // defaultPublicURL: "http://localhost:3001",
   // defaultAPIURL: "http://localhost:3001/api/v1",
-  defaultURL: "http://f5abc593.ngrok.io",
-  defaultPublicURL: "http://f5abc593.ngrok.io",
-  defaultAPIURL: `http://f5abc593.ngrok.io/api/v1`,
-  // defaultURL: "https://demoapp.my.id",
-  // defaultPublicURL: "https://api.demoapp.my.id",
-  // defaultAPIURL: "https://api.demoapp.my.id/api/v1",
+   defaultURL: "https://demoapp.my.id",
+   defaultPublicURL: "https://api.demoapp.my.id",
+   defaultAPIURL: "https://api.demoapp.my.id/api/v1"
 };
 
 module.exports = config;
