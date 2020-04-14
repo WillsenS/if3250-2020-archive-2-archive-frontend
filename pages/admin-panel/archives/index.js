1f6e017aa116fdf4f48a4cd81eb5a61083627f6b
Abda Shaffan <abdashaffan@gmail.com>
2020-04-14 10:14:01 +0700

    add crud handlers

--- pages/admin-panel/archives/index.js
+++ pages/admin-panel/archives/index.js
@@ -1,8 +1,8 @@
-import React, {useEffect, useRef, useState} from "react";
+import React, {useEffect, useState} from "react";
 import AdminLayout from "../../../src/components/Admin/Layout";
 import ArchiveTable from "../../../src/components/Admin/ArchiveTable";
 import Classification from "../../../src/scheme/Classification";
-import {postSubmitArchive} from "../../../resources/archive";
+import {postSubmitArchive, patchEditArchive, deleteArchive, getArchiveList} from "../../../resources/archive";
 
 const mockArchiveResponse = {
     currentPage: 1,
@@ -12,12 +12,16 @@ const mockArchiveResponse = {
 };
 
 export default function Archives() {
-
     const [submittedArchive, setSubmittedArchive] = useState({});
-    const firstRender = useRef(true);
-    const section = 3; // section archive
+    const [editedArchive, setEditedArchive] = useState({});
+    const [deletedArchiveId, setDeletedArchiveId] = useState('');
+    const [searchQuery, setSearchQuery] = useState('13517021'); // TODO: Jadiin empty string, sekarang apinya belum bisa query kosong
+    const [page, setPage] = useState(1);
+    const section = 3; // section: archive
 
+    // Add archive handler
     useEffect( () => {
+
         const errorText = 'Terjadi kesalahan. Silahkan coba beberapa saat lagi';
         const successText = 'Arsip berhasil ditambahkan dan disimpan';
         const submitArchive = async (archive) => {
@@ -36,15 +40,77 @@ export default function Archives() {
                 alert(errorText);
             }
         };
-        if (firstRender.current) {
+        if (!isEmptyObj(submittedArchive)) {
             // Don't run useEffect on first component load
-            firstRender.current = false;
-        } else {
             submitArchive(submittedArchive);
         }
     }, [submittedArchive]);
+    // Edit archive handler
+    useEffect( () => {
+        const errorText = 'Terjadi kesalahan. Silahkan coba beberapa saat lagi';
+        const successText = 'Arsip berhasil dirubah';
+        const editArchive = async (archive) => {
+            try {
+                const res = await patchEditArchive(archive);
+                if (res.status === 200) {
+                    // eslint-disable-next-line no-undef
+                    alert(successText);
+                } else {
+                    // eslint-disable-next-line no-undef
+                    alert(errorText);
+                }
+            }
+            catch (e) {
+                // eslint-disable-next-line no-undef
+                alert(errorText);
+            }
+        };
+        if (!isEmptyObj(editedArchive)) {
+            // Don't run useEffect on first component load
+            editArchive(editedArchive);
+        }
+    }, [editedArchive]);
+    // Delete archive handler
+    useEffect( () => {
+        const errorText = 'Terjadi kesalahan. Silahkan coba beberapa saat lagi';
+        const successText = 'Arsip berhasil dihapus';
+        const handleDeleteArchive = async (id) => {
+            try {
+                const res = await deleteArchive(id);
+                if (res.status === 200) {
+                    // eslint-disable-next-line no-undef
+                    alert(successText);
+                } else {
+                    // eslint-disable-next-line no-undef
+                    alert(errorText);
+                }
+            }
+            catch (e) {
+                // eslint-disable-next-line no-undef
+                alert(errorText);
+            }
+        };
+        if (deletedArchiveId) {
+            // Don't run useEffect on first component load
+            handleDeleteArchive(deletedArchiveId);
+        }
+    }, [deletedArchiveId]);
+    // Search handler
+    useEffect(() => {
+        const handleGetArchiveList = async (searchQuery, page) => {
+            if (searchQuery.length <= 0) return;
+            const res = await getArchiveList(searchQuery, page);
+            console.log('getArchiveList response: ');
+            console.log(res);
+        };
+        handleGetArchiveList(searchQuery, page);
+    }, [searchQuery, page]);
 
 
+
+    const isEmptyObj = obj => {
+        return Object.keys(obj).length === 0;
+    };
     const handlePageRequests = (val) => {
     };
 
@@ -60,12 +126,14 @@ export default function Archives() {
         ));
         filteredArchives.push(editedArchive);
         mockArchiveResponse.payload = [...filteredArchives];
+        setEditedArchive({...editedArchive});
     };
 
     const handleDeleteArchiveRequest = (selectedArchive) => {
         mockArchiveResponse.payload = mockArchiveResponse.payload.filter(archive => (
             archive._id !== selectedArchive._id
         ));
+        setDeletedArchiveId(selectedArchive._id);
     };
 
     return (
