95da3e069edbf0b788b1f70d8f6b3e628ed74656
Juniardi Akbar <juniardiakbar@gmail.com>
2020-04-16 20:53:25 +0700

    Change config

--- config/index.js
+++ config/index.js
@@ -2,12 +2,9 @@ const config = {
   // defaultURL: "http://localhost:3001",
   // defaultPublicURL: "http://localhost:3001",
   // defaultAPIURL: "http://localhost:3001/api/v1",
-  defaultURL: "https://923f0ae7.ngrok.io",
-  defaultPublicURL: "https://923f0ae7.ngrok.io",
-  defaultAPIURL: `https://923f0ae7.ngrok.io/api/v1`,
-  // defaultURL: "https://demoapp.my.id",
-  // defaultPublicURL: "https://api.demoapp.my.id",
-  // defaultAPIURL: "https://api.demoapp.my.id/api/v1",
+  defaultURL: "https://demoapp.my.id",
+  defaultPublicURL: "https://api.demoapp.my.id",
+  defaultAPIURL: "https://api.demoapp.my.id/api/v1",
 };
 
 module.exports = config;
