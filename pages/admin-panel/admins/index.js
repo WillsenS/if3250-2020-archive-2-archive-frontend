ace74161846564d1719dbd965ff3df513e9ff3a2
Abda Shaffan <abdashaffan@gmail.com>
2020-04-23 10:02:34 +0700

    fix bad commit

--- pages/admin-panel/admins/index.js
+++ pages/admin-panel/admins/index.js
@@ -1,159 +1,141 @@
-import React, {useEffect, useState} from "react";
+// @ts-nocheck
+import React, { useEffect, useState } from "react";
 
 import AdminLayout from "../../../src/components/Admin/Layout";
 import AdminTable from "../../../src/components/Admin/AdminTable";
 import {
-    getAdmins,
-    getNonAdmins,
-    patchEditUserRole,
-    patchResetRoleToDefault,
+  getAdmins,
+  getNonAdmins,
+  patchEditUserRole,
+  patchResetRoleToDefault,
 } from "../../../resources/user";
 import axios from "axios";
-import Archives from "../archives";
 
 function AdminUsers(props) {
-    const [admins, setAdmins] = useState([]);
-    const [users, setUsers] = useState([]);
-    const [actionSet, setActionSet] = useState({
-        userId: "",
-        roleId: 0,
-        action: 0,
-    });
-    const [page, setPage] = useState(1);
-    const [totalPages, setTotalPages] = useState(0);
-    const section = 2; //Section: Admin
-    const IDLE = 0;
-    const CHANGE_ADMIN = 1;
-    const DELETE_ADMIN = 2;
+  const [admins, setAdmins] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [actionSet, setActionSet] = useState({
+    userId: "",
+    roleId: 0,
+    action: 0,
+  });
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const [error, setError] = useState(false);
+  const section = 2; //Section: Admin
+  const IDLE = 0;
+  const CHANGE_ADMIN = 1;
+  const DELETE_ADMIN = 2;
 
-    //Update tables after CRD operation
-    const updateTables = async (role_id, pages) => {
-        const [res1, res2] = await Promise.all([
-            getAdmins(role_id, pages, props.token),
-            getNonAdmins(null, null, props.token),
-        ]);
-        if (res1.status === 200 && res2.status === 200) {
-            setAdmins(res1.data.data);
-            setUsers(res2.data.data);
-            setPage(res1.data.currentPage);
-            setTotalPages(res1.data.totalPages);
-            setActionSet({userId: "", roleId: 0, action: IDLE});
+  //Update tables after CRD operation
+  const updateTables = async (role_id, pages) => {
+    try {
+      const [res1, res2] = await Promise.all([
+        getAdmins(role_id, pages, props.token),
+        getNonAdmins(null, null, props.token),
+      ]);
+      if (res1.status === 200 && res2.status === 200) {
+        setAdmins(res1.data.data);
+        setUsers(res2.data.data);
+        setPage(res1.data.currentPage);
+        setTotalPages(res1.data.totalPages);
+        setActionSet({ userId: "", roleId: 0, action: IDLE });
+      }
+    } catch (err) {
+      setError(true);
+    }
+  };
+
+  //Change access handler
+  useEffect(() => {
+    let mounted = true;
+    let source = axios.CancelToken.source();
+    const changeAdminAccess = async (user_id, role_id, action, source) => {
+      try {
+        let res;
+        if (action === CHANGE_ADMIN) {
+          res = await patchEditUserRole(user_id, role_id, source, props.token);
+        } else if (action === DELETE_ADMIN) {
+          res = await patchResetRoleToDefault(user_id, source, props.token);
+        } else {
+          return;
         }
+        if (res.status === 200) {
+          alert("Berhasil mengubah role user");
+          setActionSet({ ...actionSet, action: IDLE });
+          await updateTables();
+        } else {
+          alert("Gagal mengubah role user");
+        }
+      } catch (e) {
+        alert("Gagal mengubah role user");
+      }
     };
-
-
-    //Change access handler
-    useEffect(() => {
-        let mounted = true;
-        let source = axios.CancelToken.source();
-        const changeAdminAccess = async (user_id, role_id, action, source) => {
-            try {
-                let res;
-                if (action === CHANGE_ADMIN) {
-                    res = await patchEditUserRole(user_id, role_id, source, props.token);
-                } else if (action === DELETE_ADMIN) {
-                    res = await patchResetRoleToDefault(user_id, source, props.token);
-                } else {
-                    return;
-                }
-                if (res.status === 200) {
-                    alert("Berhasil mengubah role user");
-                    setActionSet({...actionSet, action: IDLE});
-                    await updateTables();
-                } else {
-                    alert("Gagal mengubah role user");
-                }
-            } catch (e) {
-                alert("Gagal mengubah role user");
-            }
-        };
-        (async () => {
-            if (actionSet.action !== IDLE)
-                await changeAdminAccess(
-                    actionSet.userId,
-                    actionSet.roleId,
-                    actionSet.action,
-                    source
-                )
-        })();
-        return () => {
-            mounted = false;
-            source.cancel("Req. cancelled because user left the page");
-        };
-    }, [actionSet]);
-
-    //pagination handler
-    useEffect(() => {
-        (async () => {
-            await updateTables(null, page);
-        })();
-    }, [page]);
-
-    const handlePageRequest = (val) => {
-        setPage(parseInt(val));
+    (async () => {
+      await changeAdminAccess(
+        actionSet.userId,
+        actionSet.roleId,
+        actionSet.action,
+        source
+      );
+    })();
+    return () => {
+      mounted = false;
+      source.cancel("Req. cancelled because user left the page");
     };
+  }, [actionSet]);
 
-    const handleSearch = (val) => {
-        // TODO: Tembak Backend (route?)
-    };
+  //pagination handler
+  useEffect(() => {
+    (async () => {
+      await updateTables(null, page);
+    })();
+  }, [page]);
 
-    const handleAddNewAdminRequest = (userId, roleId) => {
-        setActionSet({userId, roleId, action: CHANGE_ADMIN});
-    };
+  const handlePageRequest = (val) => {
+    setPage(parseInt(val));
+  };
 
-    const handleDeleteAdminRequest = (userId) => {
-        setActionSet({userId, roleId: 0, action: DELETE_ADMIN});
-    };
+  const handleSearch = (val) => {
+    // TODO: Tembak Backend (route?)
+  };
 
-    const handleEditAdminRequest = (userId, roleId) => {
-        setActionSet({userId, roleId, action: CHANGE_ADMIN});
-    };
+  const handleAddNewAdminRequest = (userId, roleId) => {
+    setActionSet({ userId, roleId, action: CHANGE_ADMIN });
+  };
 
-    return (
-        <AdminLayout section={section} title="Pengaturan Data Admin">
-            {props.admins && props.users ? (
-                <AdminTable
-                    dataAdmin={admins}
-                    dataUser={users}
-                    page={page}
-                    totalPages={totalPages}
-                    handlePageRequest={handlePageRequest}
-                    handleAddNewDataRequest={handleAddNewAdminRequest}
-                    handleDeleteDataRequest={handleDeleteAdminRequest}
-                    handleEditDataRequest={handleEditAdminRequest}
-                    handleSearch={handleSearch}
-                />
-            ) : (
-                <span>{props.error}</span>
-            )}
-        </AdminLayout>
-    );
+  const handleDeleteAdminRequest = (userId) => {
+    setActionSet({ userId, roleId: 0, action: DELETE_ADMIN });
+  };
+
+  const handleEditAdminRequest = (userId, roleId) => {
+    setActionSet({ userId, roleId, action: CHANGE_ADMIN });
+  };
+
+  return (
+    <AdminLayout section={section} title="Pengaturan Data Admin">
+      {admins && users ? (
+        <AdminTable
+          dataAdmin={admins}
+          dataUser={users}
+          page={page}
+          totalPages={totalPages}
+          handlePageRequest={handlePageRequest}
+          handleAddNewDataRequest={handleAddNewAdminRequest}
+          handleDeleteDataRequest={handleDeleteAdminRequest}
+          handleEditDataRequest={handleEditAdminRequest}
+          handleSearch={handleSearch}
+        />
+      ) : (
+        <span>Terjadi kesalahan pada saat mengambil data</span>
+      )}
+    </AdminLayout>
+  );
 }
 
-// AdminUsers.getInitialProps = async () => {
-//   try {
-//     const [res1, res2] = await Promise.all([getAdmins(), getNonAdmins()]);
-//     if (res1.status === 200 && res2.status === 200) {
-//       return {
-//         admins: res1.data,
-//         users: res2.data,
-//       };
-//     } else {
-//       return {
-//         error: "Gagal memuat data admin",
-//       };
-//     }
-//   } catch (e) {
-//     return {
-//       error: "Gagal memuat data admin",
-//     };
-//   }
-// };
-
-
-Archives.getInitialProps = ({req, query}) => {
-    if (req && req.cookies) return {...query, token: req.cookies.token};
-    else return query;
+AdminUsers.getInitialProps = ({ req, query }) => {
+  if (req && req.cookies) return { ...query, token: req.cookies.token };
+  else return query;
 };
 
-export default Archives;
+export default AdminUsers;
--- resources/user.js
+++ resources/user.js
@@ -1,8 +1,16 @@
+// @ts-nocheck
 import axios from "axios";
 import { defaultAPIURL } from "../config";
 
 axios.defaults.withCredentials = true;
 
+/**
+ * Get admin list
+ * @param {number} [role_id] Admin role id, optional
+ * @param {number} [page] Page number, optional
+ * @param {object} source Axios request token
+ * @param {string} token Authentication token
+ */
 export const getAdmins = async (role_id, page, source, token) => {
   try {
     axios.defaults.headers.common = { Authorization: `Bearer ${token}` };
@@ -22,6 +30,12 @@ export const getAdmins = async (role_id, page, source, token) => {
   }
 };
 
+/**
+ * Get non-admin list
+ * @param {number} [page] Page number, optional
+ * @param {object} source Axios request token
+ * @param {string} token Authentication token
+ */
 export const getNonAdmins = async (page, source, token) => {
   try {
     axios.defaults.headers.common = { Authorization: `Bearer ${token}` };
@@ -40,8 +54,16 @@ export const getNonAdmins = async (page, source, token) => {
   }
 };
 
+/**
+ * Edit user role
+ * @param {number} user_id User id
+ * @param {number} role_id New role id
+ * @param {object} source Axios request token
+ * @param {string} token Authentication token
+ */
 export const patchEditUserRole = async (user_id, role_id, source, token) => {
   try {
+    console.log("auth: ", token);
     axios.defaults.headers.common = { Authorization: `Bearer ${token}` };
     const url = `${defaultAPIURL}/users/${user_id}`;
     let data = new FormData();
@@ -61,6 +83,12 @@ export const patchEditUserRole = async (user_id, role_id, source, token) => {
   }
 };
 
+/**
+ * Remove admin access from user, reset to default access
+ * @param {number} user_id User id
+ * @param {object} source Axios request token
+ * @param {string} token Authentication token
+ */
 export const patchResetRoleToDefault = async (user_id, source, token) => {
   try {
     axios.defaults.headers.common = { Authorization: `Bearer ${token}` };
