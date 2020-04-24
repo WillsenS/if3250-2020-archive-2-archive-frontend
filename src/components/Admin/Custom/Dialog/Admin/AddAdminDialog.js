8f6c4868a07fdf4130156bed84acdcce8ce120c7
Abda Shaffan <abdashaffan@gmail.com>
2020-04-24 10:53:08 +0700

    change wrong input label

--- src/components/Admin/Custom/Dialog/Admin/AddAdminDialog.js
+++ src/components/Admin/Custom/Dialog/Admin/AddAdminDialog.js
@@ -6,132 +6,140 @@ import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import {
-    makeStyles,
-    FormControl,
-    InputLabel,
-    Select,
-    MenuItem,
-    FormHelperText
+  makeStyles,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  FormHelperText,
 } from "@material-ui/core";
-import Role from '../../../../../scheme/Unit';
+import Role from "../../../../../scheme/Unit";
 
 //PropTypes validation
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-const useStyles = makeStyles(theme => ({
-    root: {
-        minWidth: "500px"
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minWidth: "500px",
+  },
+  title: {
+    color: "#1aaa55",
+  },
+  buttonCancel: {
+    color: "#333",
+  },
+  buttonExecute: {
+    color: theme.palette.getContrastText("#1aaa55"),
+    backgroundColor: "#1aaa55",
+    borderColor: "#168f48",
+    "&:hover": {
+      backgroundColor: "#168f48",
+      borderColor: "#12753a",
     },
-    title: {
-        color: "#1aaa55"
-    },
-    buttonCancel: {
-        color: "#333"
-    },
-    buttonExecute: {
-        color: theme.palette.getContrastText("#1aaa55"),
-        backgroundColor: "#1aaa55",
-        borderColor: "#168f48",
-        "&:hover": {
-            backgroundColor: "#168f48",
-            borderColor: "#12753a"
-        }
-    },
-    formControl: {
-        margin: theme.spacing(1),
-        minWidth: 120,
-        maxWidth: 300
-    },
-    selectEmpty: {
-        marginTop: theme.spacing(2)
-    }
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+    maxWidth: 300,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
 }));
 
 export default function AddAdminDialog(props) {
-    const [userId, setUserId] = React.useState("");
-    const [roleId, setRoleId] = React.useState("");
-    const classes = useStyles();
-    const {open, handleClose, userList, handleAddNewDataRequest} = props;
+  const [userId, setUserId] = React.useState("");
+  const [roleId, setRoleId] = React.useState("");
+  const classes = useStyles();
+  const { open, handleClose, userList, handleAddNewDataRequest } = props;
 
-    const handleNameChange = event => {
-        setUserId(event.target.value);
-    };
+  const handleNameChange = (event) => {
+    setUserId(event.target.value);
+  };
 
-    const handleUnitChange = event => {
-        setRoleId(event.target.value);
-    };
+  const handleUnitChange = (event) => {
+    setRoleId(event.target.value);
+  };
 
-    const handleAddData = () => {
-        handleAddNewDataRequest(userId,roleId);
-        setUserId("");
-        setRoleId("");
-        handleClose();
-    };
-    return (
-        <>
-            <Dialog
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="add-new-admin"
-                aria-describedby="form-to-select-new-admin"
-                maxWidth="md"
-                classes={{root: classes.root}}
-                disableBackdropClick
+  const handleAddData = () => {
+    handleAddNewDataRequest(userId, roleId);
+    setUserId("");
+    setRoleId("");
+    handleClose();
+  };
+  return (
+    <>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="add-new-admin"
+        aria-describedby="form-to-select-new-admin"
+        maxWidth="md"
+        classes={{ root: classes.root }}
+        disableBackdropClick
+      >
+        <DialogTitle classes={{ root: classes.title }}>
+          Tambah Admin
+        </DialogTitle>
+        <DialogContent dividers>
+          <FormControl required className={classes.formControl}>
+            <InputLabel id="user-label">User</InputLabel>
+            <Select
+              labelId="pilih-user"
+              id="pilih-user-admin-baru"
+              value={userId}
+              onChange={handleNameChange}
             >
-                <DialogTitle classes={{root: classes.title}}>
-                    Tambah Admin
-                </DialogTitle>
-                <DialogContent dividers>
-                    <FormControl required className={classes.formControl}>
-                        <InputLabel id="user-label">User</InputLabel>
-                        <Select
-                            labelId="pilih-user"
-                            id="pilih-user-admin-baru"
-                            value={userId}
-                            onChange={handleNameChange}
-                        >
-                            {
-                                //List all users
-                                userList.map((user) =>
-                                    <MenuItem value={user._id} key={user._id}>{user.fullname}</MenuItem>
-                                )
-                            };
-                        </Select>
-                        <FormHelperText>
-                            Nama user yang akan diberikan akses admin
-                        </FormHelperText>
-                    </FormControl>
-                    <FormControl required className={classes.formControl}>
-                        <InputLabel id="pilih-unit-kerja">Unit Kerja</InputLabel>
-                        <Select
-                            labelId="pilih-unit-kerja"
-                            id="pilih-unit-kerja"
-                            value={roleId}
-                            onChange={handleUnitChange}
-                        >
-                            {
-                                Role.map(r => (
-                                    <MenuItem value={r._id} key={r._id}>{r.label}</MenuItem>
-                                ))
-                            }
-                        </Select>
-                        <FormHelperText>Fakultas dari user yang dipilih</FormHelperText>
-                    </FormControl>
-                    <DialogActions>
-                        <Button color="default" onClick={handleClose}>
-                            Batal
-                        </Button>
-                        <Button classes={{root: classes.buttonExecute}} onClick={handleAddData}>Tambahkan</Button>
-                    </DialogActions>
-                </DialogContent>
-            </Dialog>
-        </>
-    );
+              {
+                //List all users
+                userList.map((user) => (
+                  <MenuItem value={user._id} key={user._id}>
+                    {user.fullname}
+                  </MenuItem>
+                ))
+              }
+              ;
+            </Select>
+            <FormHelperText>
+              Nama user yang akan diberikan akses admin
+            </FormHelperText>
+          </FormControl>
+          <FormControl required className={classes.formControl}>
+            <InputLabel id="pilih-unit-kerja">Unit Kerja</InputLabel>
+            <Select
+              labelId="pilih-unit-kerja"
+              id="pilih-unit-kerja"
+              value={roleId}
+              onChange={handleUnitChange}
+            >
+              {Role.map((r) => (
+                <MenuItem value={r._id} key={r._id}>
+                  {r.label}
+                </MenuItem>
+              ))}
+            </Select>
+            <FormHelperText>Unit Kerja dari user yang dipilih</FormHelperText>
+          </FormControl>
+          <DialogActions>
+            <Button color="default" onClick={handleClose}>
+              Batal
+            </Button>
+            <Button
+              classes={{ root: classes.buttonExecute }}
+              onClick={handleAddData}
+            >
+              Tambahkan
+            </Button>
+          </DialogActions>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
 }
 
 AddAdminDialog.propTypes = {
-    open: PropTypes.bool,
-    handleClose: PropTypes.func,
-    userList: PropTypes.array,
-    handleAddNewDataRequest: PropTypes.func
+  open: PropTypes.bool,
+  handleClose: PropTypes.func,
+  userList: PropTypes.array,
+  handleAddNewDataRequest: PropTypes.func,
 };
--- src/components/Admin/Custom/Dialog/Admin/DeleteConfirmDialog.js
+++ src/components/Admin/Custom/Dialog/Admin/DeleteConfirmDialog.js
@@ -6,88 +6,79 @@ import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
-import {makeStyles} from "@material-ui/core";
-import PropTypes from 'prop-types';
+import { makeStyles } from "@material-ui/core";
+import PropTypes from "prop-types";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import ListItemText from "@material-ui/core/ListItemText";
 import ListItem from "@material-ui/core/ListItem";
 import List from "@material-ui/core/List";
 
-
-const useStyles = makeStyles(theme => ({
-    remove: {
-        backgroundColor: theme.palette.getContrastText("#cb2431"),
-        color: "#cb2431"
-    }
+const useStyles = makeStyles((theme) => ({
+  remove: {
+    backgroundColor: theme.palette.getContrastText("#cb2431"),
+    color: "#cb2431",
+  },
 }));
 
 export default function DeleteConfirmDialog(props) {
-    const {open, handleClose, handleDelete, data} = props;
-    const classes = useStyles();
+  const { open, handleClose, handleDelete, data } = props;
+  const classes = useStyles();
 
-    const handleDeleteClick = (admin_id) => {
-        handleDelete(admin_id);
-        handleClose();
-    };
+  const handleDeleteClick = (admin_id) => {
+    handleDelete(admin_id);
+    handleClose();
+  };
 
-    return (
-        <>
-            <Dialog
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="delete-confirmation-alert"
-                aria-describedby="Confirmation-popup-for-deletion"
-            >
-                <DialogTitle id="alert-dialog-title" style={{color: "#cb2431"}}>
-                    Konfirmasi Penghapusan Akses Admin
-                </DialogTitle>
-                <DialogContent>
-                    <DialogContentText>Anda yakin ingin menghapus akses admin untuk user ini?</DialogContentText>
-                    <List
-                        component="div"
-                        role="list">
-                        <ListItem divider role="listitem">
-                            <ListItemText
-                                primary="Nama Admin"
-                                secondary={data.fullname}
-                            />
-                        </ListItem>
-                        <ListItem divider role="listitem">
-                            <ListItemText
-                                primary="Akses Admin untuk Fakultas/Sekolah"
-                                secondary={data.role}
-                            />
-                        </ListItem>
-                    </List>
-                </DialogContent>
-                <DialogActions>
-                    <Button
-                        onClick={handleClose}
-                        color="default"
-                        size="small"
-                    >
-                        Batalkan
-                    </Button>
-                    <Button
-                        onClick={() => {
-                            handleDeleteClick(data._id)
-                        }}
-                        color="default"
-                        classes={{root: classes.remove}}
-                        size="small"
-                    >
-                        Hapus
-                    </Button>
-                </DialogActions>
-            </Dialog>
-        </>
-    );
+  return (
+    <>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="delete-confirmation-alert"
+        aria-describedby="Confirmation-popup-for-deletion"
+      >
+        <DialogTitle id="alert-dialog-title" style={{ color: "#cb2431" }}>
+          Konfirmasi Penghapusan Akses Admin
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Anda yakin ingin menghapus akses admin untuk user ini?
+          </DialogContentText>
+          <List component="div" role="list">
+            <ListItem divider role="listitem">
+              <ListItemText primary="Nama Admin" secondary={data.fullname} />
+            </ListItem>
+            <ListItem divider role="listitem">
+              <ListItemText
+                primary="Akses Admin untuk Unit Kerja"
+                secondary={data.role}
+              />
+            </ListItem>
+          </List>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="default" size="small">
+            Batalkan
+          </Button>
+          <Button
+            onClick={() => {
+              handleDeleteClick(data._id);
+            }}
+            color="default"
+            classes={{ root: classes.remove }}
+            size="small"
+          >
+            Hapus
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
 }
 
-
 DeleteConfirmDialog.propTypes = {
-    open: PropTypes.bool,
-    handleClose: PropTypes.func,
-    data: PropTypes.object,
-    handleDelete: PropTypes.func
+  open: PropTypes.bool,
+  handleClose: PropTypes.func,
+  data: PropTypes.object,
+  handleDelete: PropTypes.func,
 };
