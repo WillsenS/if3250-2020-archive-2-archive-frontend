26076fa0ed02247561095247986914fdc8562578
Abda Shaffan <abdashaffan@gmail.com>
2020-04-24 10:16:41 +0700

    Add all work unit

--- src/scheme/Unit.js
+++ src/scheme/Unit.js
@@ -1,64 +1,224 @@
 const Role = [
-    {
-        label: 'Super Admin',
-        _id: 1
-    },
-    {
-        label: 'User',
-        _id: 2
-    },
-    {
-        label: "FITB",
-        _id: 3
-    },
-    {
-        label: "FMIPA",
-        _id: 4
-    },
-    {
-        label: "FSRD",
-        _id: 5
-    },
-    {
-        label: "FTI",
-        _id: 6
-    },
-    {
-        label: "FTMD",
-        _id: 7
-    },
-    {
-        label: "FTTM",
-        _id: 8,
-    },
-    {
-        label: "FTSL",
-        _id: 9,
-    },
-    {
-        label: "SAPPK",
-        _id: 10,
-    },
-    {
-        label: "SBM",
-        _id: 11,
-    },
-    {
-        label: "SF",
-        _id: 12,
-    },
-    {
-        label: "SITH-S",
-        _id: 13,
-    },
-    {
-        label: "SITH-R",
-        _id: 14,
-    },
-    {
-        label: "STEI",
-        _id: 15,
-    }
+  {
+    label: "Super Admin",
+    _id: 1,
+  },
+  {
+    label: "User",
+    _id: 2,
+  },
+  {
+    label: "FITB",
+    _id: 3,
+  },
+  {
+    label: "FMIPA",
+    _id: 4,
+  },
+  {
+    label: "FSRD",
+    _id: 5,
+  },
+  {
+    label: "FTI",
+    _id: 6,
+  },
+  {
+    label: "FTMD",
+    _id: 7,
+  },
+  {
+    label: "FTTM",
+    _id: 8,
+  },
+  {
+    label: "FTSL",
+    _id: 9,
+  },
+  {
+    label: "SAPPK",
+    _id: 10,
+  },
+  {
+    label: "SBM",
+    _id: 11,
+  },
+  {
+    label: "SF",
+    _id: 12,
+  },
+  {
+    label: "SITH-S",
+    _id: 13,
+  },
+  {
+    label: "SITH-R",
+    _id: 14,
+  },
+  {
+    label: "STEI",
+    _id: 15,
+  },
+  {
+    label: "DITRENC",
+    _id: 16,
+  },
+  {
+    label: "DIERRENC/DIRKEU",
+    _id: 17,
+  },
+  {
+    label: "LLH",
+    _id: 18,
+  },
+  {
+    label: "DIT AU",
+    _id: 19,
+  },
+  {
+    label: "DITPEG",
+    _id: 20,
+  },
+  {
+    label: "DITPEG/UPT PMO",
+    _id: 21,
+  },
+  {
+    label: "DITKHI",
+    _id: 22,
+  },
+  {
+    label: "DIT HUA",
+    _id: 23,
+  },
+  {
+    label: "DIT HUA/DIT AU",
+    _id: 24,
+  },
+  {
+    label: "DIT BANG",
+    _id: 25,
+  },
+  {
+    label: "DIT RENC",
+    _id: 26,
+  },
+  {
+    label: "DITLOG",
+    _id: 27,
+  },
+  {
+    label: "DIT SP",
+    _id: 28,
+  },
+  {
+    label: "DITRENC/DITKEU",
+    _id: 29,
+  },
+  {
+    label: "DITKEU",
+    _id: 30,
+  },
+  {
+    label: "DITLOG/DITKEU",
+    _id: 31,
+  },
+  {
+    label: "DITKEU/DITPEG",
+    _id: 32,
+  },
+  {
+    label: "DITKEU/DITLOG/DITPEG",
+    _id: 33,
+  },
+  {
+    label: "DITKEU/SPI",
+    _id: 34,
+  },
+  {
+    label: "DITSP",
+    _id: 35,
+  },
+  {
+    label: "UPT K3L",
+    _id: 36,
+  },
+  {
+    label: "DITPEG/DIT AU",
+    _id: 37,
+  },
+  {
+    label: "DIT AU/DITPEG",
+    _id: 38,
+  },
+  {
+    label: "DITSTI",
+    _id: 39,
+  },
+  {
+    label: "DIT STI/E-Learning",
+    _id: 40,
+  },
+  {
+    label: "UPT PMO",
+    _id: 41,
+  },
+  {
+    label: "UPT PMO/DITPEG",
+    _id: 42,
+  },
+  {
+    label: "LPPM",
+    _id: 43,
+  },
+  {
+    label: "SPI",
+    _id: 44,
+  },
+  {
+    label: "DITDIK",
+    _id: 46,
+  },
+  {
+    label: "BPUDL",
+    _id: 47,
+  },
+  {
+    label: "UUP Penerbit",
+    _id: 48,
+  },
+  {
+    label: "UPT Perpustakaan",
+    _id: 49,
+  },
+  {
+    label: "Lab/Studio",
+    _id: 50,
+  },
+  {
+    label: "DT HUA",
+    _id: 51,
+  },
+  {
+    label: "LPIK",
+    _id: 52,
+  },
+  {
+    label: "MWA",
+    _id: 53,
+  },
+  {
+    label: "SA",
+    _id: 54,
+  },
+  {
+    label: "FGB",
+    _id: 55,
+  },
+  {
+    label: "Komite Audit",
+    _id: 56,
+  },
 ];
 
 export default Role;
